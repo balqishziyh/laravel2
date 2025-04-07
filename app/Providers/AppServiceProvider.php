b6cd00e139628ff03f67bb0b4dfb7ab3abd56ecb
Haziyah Ariff <23ftt1187@student.pb.edu.bn>
2025-04-07 08:56:32 +0800

    update appserviceproviders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,20 +13,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // You can add any necessary registrations here if needed
     }
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-        return (new \Illuminate\Notifications\Messages\MailMessage)
-            ->subject('Verify Your Email Address')
-            ->line('Click the button below to verify your email address.')
-            ->action('Verify Email', $url)
-            ->salutation(new HtmlString('Best regards,<br>Haziyah'));
-    });
-}
+    {
+        // Customizing the email verification message
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new \Illuminate\Notifications\Messages\MailMessage)
+                ->subject('Verify Your Email Address')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email', $url)
+                ->salutation(new HtmlString('Best regards,<br>Haziyah'));
+        });
+    }
 }
